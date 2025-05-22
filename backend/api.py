5f5688205476b668658f318da8564214b8e5b588
UmerAhmad11 <sumerahmad2002@gmail.com>
2025-05-22 18:11:03 +0300

    Added Buy and Sell Routes

--- backend/api.py
+++ backend/api.py
@@ -3,8 +3,9 @@ from routers import trading
 
 app = FastAPI()
 
+app.include_router(trading.router, prefix="/api", tags=["Trading"])
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to the main page!"}
 
-app.include_router(trading.router)
--- backend/routers/trading.py
+++ backend/routers/trading.py
@@ -5,3 +5,13 @@ router = APIRouter()
 @router.get("/trade")
 async def get_trade():
     return {"message": "Trade endpoint is live!"}
+
+@router.post("/buy")
+
+async def buy():
+    return {"message": "Buy endpoint is live!"}
+
+@router.post("/sell")
+
+async def sell():
+    return {"message": "Sell endpoint is live!"}
