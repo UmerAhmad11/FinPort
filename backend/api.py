01c823f97e629159a06ff2b4a21d6da3264c371b
UmerAhmad11 <sumerahmad2002@gmail.com>
2025-05-26 15:41:42 +0300

    added router to api.py

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import trading, auth, frontpage, deposit  # import your trading endpoints
+from routers import trading, auth, frontpage, deposit, purchase_tracker  # import your trading endpoints
 
 # Create the FastAPI app instance
 app = FastAPI()
@@ -16,9 +16,10 @@ app.add_middleware(
 
 # Register the trading router under /api
 app.include_router(trading.router, prefix="/api", tags=["Trading"])
-app.include_router(auth.router, prefix="/api", tags=["Auth"])
-app.include_router(frontpage.router, prefix="/api", tags=["Frontpage"])
+app.include_router(auth.router, prefix="/main", tags=["Auth"])
+app.include_router(frontpage.router, prefix="/main", tags=["Frontpage"])
 app.include_router(deposit.router, prefix="/api", tags=["Deposit"])
+app.include_router(purchase_tracker.router, prefix="/api", tags=["Purchase Tracker"])
 
 
 # Basic root endpoint for testing
